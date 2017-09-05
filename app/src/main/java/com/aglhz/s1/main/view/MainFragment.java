747caeba393ec1d613085cda98591d9a547c33a5
langmanleguang <langmanleguang@qq.com>
2017-09-05 13:12:32 +0800

    add settings.

--- app/src/main/java/com/aglhz/s1/main/view/MainFragment.java
+++ app/src/main/java/com/aglhz/s1/main/view/MainFragment.java
@@ -207,8 +207,6 @@ public class MainFragment extends BaseFragment implements EasyPermissions.Permis
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        // Forward results to EasyPermissions
         EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
