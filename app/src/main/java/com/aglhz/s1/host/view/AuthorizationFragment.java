7e2047555f422a024242cbaa65f82c95e6c2ef8a
langmanleguang <langmanleguang@qq.com>
2017-09-07 08:16:32 +0800

    add push.

--- app/src/main/java/com/aglhz/s1/host/view/AuthorizationFragment.java
+++ app/src/main/java/com/aglhz/s1/host/view/AuthorizationFragment.java
@@ -3,6 +3,7 @@ package com.aglhz.s1.host.view;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -130,8 +131,14 @@ public class AuthorizationFragment extends BaseFragment<AuthorizationContract.Pr
         adapter.setOnItemChildClickListener((adapter1, view, position) -> {
             switch (view.getId()) {
                 case R.id.tv_unbound:
-                    params.fid = adapter.getItem(position).getFid();
-                    mPresenter.requestGatewayUnAuth(params);
+                    new AlertDialog.Builder(_mActivity)
+                            .setTitle("提醒")
+                            .setMessage("是否解除对该用户授权")
+                            .setNegativeButton("否", null)
+                            .setPositiveButton("是", (dialog, which) -> {
+                                params.fid = adapter.getItem(position).getFid();
+                                mPresenter.requestGatewayUnAuth(params);
+                            }).show();
                     break;
             }
         });
